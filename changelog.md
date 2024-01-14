b32ef668106cf131fa5ac78bc0cb5e2e2df4e728
Ismael J. Varela <info@varelait.com>
2024-01-14 19:46:41 -0400

    new salary input

--- changelog.md
+++ changelog.md
@@ -1,1 +1,7 @@
 # Change Log
+
+## New Salary Input
+
+_14 Ene 2024_
+
+There are two way salary input; by a fixed format or by monthly comission.
--- src/index.ts
+++ src/index.ts
@@ -13,16 +13,46 @@ const WELCOME = `
 `;
 
 
-function calculaPestaciones () {
+
+function getSalaries(): number| false{
+    const fixed = Readline.question('Sueldo fijo: [Si/No] ', {limit: ['Si', 'No']}).match(/s/i)? true : false;
+    
+    if(fixed){
+        const salarioStr = Readline.question('Salario mensual: ');
+        return parseFloat(salarioStr.replace(/,/, ''));
+    }
+
+    let salaries: number[]= [];
+
+    for(let i= 0; i < 10; i++){
+        const salary= parseFloat(Readline.question(Salario ${i + 1}:));
+        
+        if(!Number.isNaN(salary))
+            salaries.push(salary);
+        else
+            break;
+    }
+
+    if(salaries.length < 0)
+        return false;
+
+    return salaries.reduce((first, second)=> (first + second), 0) / salaries.length;
+}
+
+
+
+function calculateBenefits () {
     let prestaciones: number= 0;
     console.log('Complete el formulario para calcular sus prestaciones labores.\n');
-
     const name = Readline.question('Nombre: ');
-    const salarioStr = Readline.question('Salario mensual: ');
-    const salary =  parseFloat(salarioStr.replace(/,/, ''));
     const startDate = Readline.question('Fecha de inicio de labores: ');
     const endDate = Readline.question('Fecha de Terminación: ');
+    const salary= getSalaries();
+    
+    if(!salary)
+        return;
 
+    console.log(Su salario promedio es de: RD$ ${CURRENCY.format(salary)}\n);
     const calculator= new CalcPestaciones(name, salary, startDate, endDate);
     //const calculator= new CalcPestaciones('Ismael', 33333, '2016-7-31', '2023-6-28');
 
@@ -30,11 +60,13 @@ function calculaPestaciones () {
         prestaciones+= calculator.calcPreaviso();
         console.log('Monto de preaviso: RD' + CURRENCY.format(calculator.calcPreaviso()));
     }
+
     if(Readline.question('¿Desea incluir la cesantia? [Si/No] ', {limit: ['Si', 'No']} ).match(/s/i)? true : false){
         prestaciones+= calculator.calcCesantia();
         console.log('Monto de cesantia: RD' + CURRENCY.format(calculator.calcCesantia()));
         
     }
+
     if(Readline.question('¿Desea incluir las vacaciones? [Si/No] ', {limit: ['Si', 'No']} ).match(/s/i)? true : false){
         prestaciones+= calculator.calcVacaciones(true);
         console.log('Monto de vacaciones: RD' + CURRENCY.format(calculator.calcVacaciones(true)));
@@ -42,6 +74,7 @@ function calculaPestaciones () {
         prestaciones+= calculator.calcVacaciones(false);
         console.log('Monto de vacaciones: RD' + CURRENCY.format(calculator.calcVacaciones(false)));
     }
+
     if(Readline.question('¿Desea incluir El salario trece? [Si/No] ', {limit: ['Si', 'No']} ).match(/s/i)? true : false){
         prestaciones+= calculator.calcExtraSalary();
         console.log('Salario trece: RD' + CURRENCY.format(calculator.calcExtraSalary()));
@@ -59,11 +92,4 @@ while(true){
     calculaPestaciones();
 }
 
-/*jj
-const mycalc= new CalcPestaciones('El sujeto', 10000, '1/5/2020', '27/11/2023');
-console.log('preaviso: RD'+ CURRENCY.format(mycalc.calcPreaviso()));
-console.log('Cesantia: RD'+ CURRENCY.format(mycalc.calcCesantia()));
-console.log('Vacaciones: RD'+ CURRENCY.format(mycalc.calcVacaciones(false)));
-console.log('Extra: RD'+ CURRENCY.format(mycalc.calcExtraSalary()));
-*/
 process.exit();
